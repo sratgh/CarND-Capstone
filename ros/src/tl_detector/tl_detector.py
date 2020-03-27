2a77e421d2111474a7213b21d889633881611cf7
StephanStu <stephan.studener@googlemail.com>
2020-03-27 06:30:27 -0700

    fix: commented tl_classifier out to make it work until simon's classifier is ready and integrated.

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -7,7 +7,7 @@ from styx_msgs.msg import Lane, Waypoint
 from scipy.spatial import KDTree
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
-from light_classification.tl_classifier import TLClassifier
+#from light_classification.tl_classifier import TLClassifier
 import tf
 import cv2
 import yaml
@@ -68,7 +68,7 @@ class TLDetector(object):
         self.stop_line_positions = self.config['stop_line_positions']
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
         self.bridge = CvBridge()
-        self.light_classifier = TLClassifier()
+        #self.light_classifier = TLClassifier()
         self.listener = tf.TransformListener()
         self.state = TrafficLight.UNKNOWN
         self.last_state = TrafficLight.UNKNOWN

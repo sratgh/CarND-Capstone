e5f7f9e8978c0c75fa1b045e0253ae41bb9258ca
Simon <simon.rudolph@mail.de>
2020-03-28 18:37:17 +0000

    feat: Add classifier to be used for transfer learning of a mobile net

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,3 +1,7 @@
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+import matplotlib
+matplotlib.use('Agg')
 #from styx_msgs.msg import TrafficLight
 import cv2
 import numpy as np
@@ -6,6 +10,7 @@ from styx_msgs.msg import TrafficLight
 import collections
 
 import tensorflow as tf
+
 import matplotlib.pyplot as plt
 from PIL import Image
 from PIL import ImageDraw
@@ -13,7 +18,22 @@ from PIL import ImageColor
 import time
 from scipy.stats import norm
 
-#plt.style.use('ggplot')
+# Import everything needed to edit/save/watch video clips
+#import imageio
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
+import IPython.display as display
+
+plt.style.use('ggplot')
+
+from keras.preprocessing.image import ImageDataGenerator
+#from keras_applications.mobilenet import MobileNetV2
+from keras.models import Model
+from keras.applications import MobileNet
+from keras.layers import GlobalAveragePooling2D, Dense
+import os
+import pathlib
+
 
 class TLClassifier(object):
     """
@@ -49,13 +69,169 @@ class TLClassifier(object):
 
         # Frozen inference graph files. NOTE: change the path to where you saved the models.
         self.SSD_GRAPH_FILE = 'ssd_mobilenet_v1_coco_11_06_2017/frozen_inference_graph.pb'
-        self.RFCN_GRAPH_FILE = 'rfcn_resnet101_coco_11_06_2017/frozen_inference_graph.pb'
-        self.FASTER_RCNN_GRAPH_FILE = 'faster_rcnn_inception_resnet_v2_atrous_coco_11_06_2017/frozen_inference_graph.pb'
+        #self.RFCN_GRAPH_FILE = 'rfcn_resnet101_coco_11_06_2017/frozen_inference_graph.pb'
+        #self.FASTER_RCNN_GRAPH_FILE = 'faster_rcnn_inception_resnet_v2_atrous_coco_11_06_2017/frozen_inference_graph.pb'
         # Colors (one for each class)
         cmap = ImageColor.colormap
-        print("Number of colors =", len(cmap))
+        #print("Number of colors =", len(cmap))
         self.COLOR_LIST = sorted([c for c in cmap.keys()])
 
+        self.detection_graph = self.load_graph(self.SSD_GRAPH_FILE)
+        # detection_graph = load_graph(RFCN_GRAPH_FILE)
+        # detection_graph = load_graph(FASTER_RCNN_GRAPH_FILE)
+
+        # The input placeholder for the image.
+        # `get_tensor_by_name` returns the Tensor with the associated name in the Graph.
+        self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+
+        # Each box represents a part of the image where a particular object was detected.
+        self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+
+        # The classification of the object (integer id).
+        self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+
+    # def load_images(self, data_dir):
+    #
+    #     data_dir = pathlib.Path(data_dir)
+    #     image_count = len(list(data_dir.glob('*.jpg')))
+    #     print("image_count", image_count)
+    #     CLASS_NAMES = np.array([item.name for item in data_dir.glob('*') if item.name != "LICENSE.txt"])
+    #     print("CLASS_NAMES", CLASS_NAMES)
+
+        ## Only with tensorflow >1.5
+        #list_ds = tf.data.Dataset.list_files(str(data_dir/'*/*'))
+
+        # return list_ds
+
+    # def get_label(file_path):
+    #   # convert the path to a list of path components
+    #   parts = tf.strings.split(file_path, os.path.sep)
+    #   # The second to last is the class-directory
+    #   return parts[-2] == CLASS_NAMES
+
+    # def decode_img(img):
+    #     IMG_HEIGHT = 224
+    #     IMG_WIDTH = 224
+    #     # convert the compressed string to a 3D uint8 tensor
+    #     img = keras.preprocessing.image.decode_jpeg(img, channels=3)
+    #     # Use `convert_image_dtype` to convert to floats in the [0,1] range.
+    #     img = keras.preprocessing.image.convert_image_dtype(img, tf.float32)
+    #     # resize the image to the desired size.
+    #     return keras.preprocessing.image.resize(img, [IMG_WIDTH, IMG_HEIGHT])
+    #
+    # def process_path(file_path):
+    #     label = get_label(file_path)
+    #     # load the raw data from the file as a string
+    #     img = tf.io.read_file(file_path)
+    #     img = decode_img(img)
+    #     return img, label
+
+    def load_and_train(self, data_dir):
+        '''
+        '''
+
+        # Constants
+        BATCH_SIZE = 32
+        IMG_HEIGHT = 224
+        IMG_WIDTH = 224
+
+        # Folder structure needs to be data/<class>/*.png
+        data_dir = pathlib.Path(data_dir)
+        image_count = len(list(data_dir.glob('*.jpg')))
+        print("image_count", image_count)
+
+        # Get classes/labels from folder structure
+        self.CLASS_NAMES = np.array([item.name for item in data_dir.glob('*') if item.name != "LICENSE.txt"])
+        print("CLASS_NAMES", self.CLASS_NAMES)
+
+
+        # Create train generator
+        train_datagen = ImageDataGenerator(rescale=1./255,
+                                            shear_range=0.2,
+                                            zoom_range=0.2,
+                                            horizontal_flip=True)
+
+        # Create test generator
+        test_datagen = ImageDataGenerator(rescale=1./255)
+
+
+        STEPS_PER_EPOCH = np.ceil(image_count/BATCH_SIZE)
+
+        train_data_gen = train_datagen.flow_from_directory(directory=str(data_dir),
+                                                     batch_size=BATCH_SIZE,
+                                                     shuffle=True,
+                                                     target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                     classes = list(self.CLASS_NAMES))
+
+        test_data_gen = test_datagen.flow_from_directory(directory="test/",
+                                                     batch_size=BATCH_SIZE,
+                                                     shuffle=True,
+                                                     target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                     classes = list(self.CLASS_NAMES))
+
+        base_model=MobileNet(input_shape=(224,224,3),weights='imagenet',include_top=False) #imports the mobilenet model and discards the last 1000 neuron layer.
+
+        x=base_model.output
+        x=GlobalAveragePooling2D()(x)
+        x=Dense(1024,activation='relu')(x) #we add dense layers so that the model can learn more complex functions and classify for better results.
+        x=Dense(1024,activation='relu')(x) #dense layer 2
+        x=Dense(512,activation='relu')(x) #dense layer 3
+        preds=Dense(4,activation='softmax')(x) #final layer with softmax activation
+
+        model=Model(inputs=base_model.input,outputs=preds)
+
+        # Print architecture
+        for i,layer in enumerate(model.layers):
+            print(i,layer.name)
+
+        for layer in model.layers:
+            layer.trainable=False
+        # or if we want to set the first 20 layers of the network to be non-trainable
+        # for layer in model.layers[:20]:
+        #     layer.trainable=False
+        # for layer in model.layers[20:]:
+        #     layer.trainable=True
+        model.compile(optimizer='Adam',loss='categorical_crossentropy',metrics=['accuracy'])
+        # Adam optimizer
+        # loss function will be categorical cross entropy
+        # evaluation metric will be accuracy
+
+        step_size_train=train_data_gen.n//train_data_gen.batch_size
+        model.fit_generator(generator=train_data_gen,
+                           steps_per_epoch=step_size_train,
+                           epochs=10,
+                           validation_data=test_data_gen,
+                           validation_steps=800)
+
+        # model.fit_generator(train_generator,
+        #                     steps_per_epoch=2000,
+        #                     epochs=50,
+        #                     validation_data=validation_generator,
+        #                     validation_steps=800)
+
+        #image_batch, label_batch = next(train_data_gen)
+        #self.show_batch(image_batch, label_batch)
+        #plt.show()
+
+    def show_batch(self, image_batch, label_batch):
+        plt.figure(figsize=(10,10))
+        for n in range(25):
+            ax = plt.subplot(5,5,n+1)
+            plt.imshow(image_batch[n])
+            plt.title(self.CLASS_NAMES[label_batch[n]==1][0].title())
+            plt.axis('off')
+            #plt.imsave("image_batch_"+str(n),image_batch[n])
+
+    # def load_labels(self, filename):
+    #     labels=[]
+    #     f = open(filename, 'r')
+    #     for line in f:
+    #         labels.append(int(line[:-2]))
+    #     return labels
 
 
     def get_classification(self, image):
@@ -97,20 +273,32 @@ class TLClassifier(object):
         x = tf.layers.batch_normalization(x)
         return tf.nn.relu(x)
 
-    # TODO: implement MobileNet conv block
     def mobilenet_conv_block(self, x, kernel_size, output_channels):
         """
         Depthwise Conv -> Batch Norm -> ReLU -> Pointwise Conv -> Batch Norm -> ReLU
         """
-        pass
+        # assumes BHWC format
+        input_channel_dim = x.get_shape().as_list()[-1]
+        W = tf.Variable(tf.truncated_normal((kernel_size, kernel_size, input_channel_dim, 1)))
 
-    def run(self):
+        # depthwise conv
+        x = tf.nn.depthwise_conv2d(x, W, (1, 2, 2, 1), padding='SAME')
+        x = tf.layers.batch_normalization(x)
+        x = tf.nn.relu(x)
+
+        # pointwise conv
+        x = tf.layers.conv2d(x, output_channels, (1, 1), padding='SAME')
+        x = tf.layers.batch_normalization(x)
+
+        return tf.nn.relu(x)
+
+    def compare_parameters(self):
         # constants but you can change them so I guess they're not so constant :)
         INPUT_CHANNELS = 32
         OUTPUT_CHANNELS = 512
         KERNEL_SIZE = 3
-        IMG_HEIGHT = 256
-        IMG_WIDTH = 256
+        IMG_HEIGHT = 225
+        IMG_WIDTH = 300
 
         with tf.Session(graph=tf.Graph()) as sess:
             # input
@@ -148,24 +336,6 @@ class TLClassifier(object):
                                                      total_mobile_params))
 
 
-            detection_graph = load_graph(self.SSD_GRAPH_FILE)
-            # detection_graph = load_graph(RFCN_GRAPH_FILE)
-            # detection_graph = load_graph(FASTER_RCNN_GRAPH_FILE)
-
-            # The input placeholder for the image.
-            # `get_tensor_by_name` returns the Tensor with the associated name in the Graph.
-            image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-
-            # Each box represents a part of the image where a particular object was detected.
-            detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-
-            # Each score represent how level of confidence for each of the objects.
-            # Score is shown on the result image, together with the class label.
-            detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
-
-            # The classification of the object (integer id).
-            detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
-
     #
     # Utility funcs
     #
@@ -199,12 +369,20 @@ class TLClassifier(object):
 
     def draw_boxes(self, image, boxes, classes, thickness=4):
         """Draw bounding boxes on the image"""
+        #image = Image.fromarray(image)
+        ###
+        # For reversing the operation:
+        # im_np = np.asarray(im_pil)
+        ###
         draw = ImageDraw.Draw(image)
         for i in range(len(boxes)):
             bot, left, top, right = boxes[i, ...]
             class_id = int(classes[i])
             color = self.COLOR_LIST[class_id]
             draw.line([(left, top), (left, bot), (right, bot), (right, top), (left, top)], width=thickness, fill=color)
+
+            #image = cv2.rectangle(image, start_point, end_point, color, thickness)
+            #cv2.rectangle(image, (left, top), (right, bot), (128,128,128), 2)
 
     def load_graph(self, graph_file):
         """Loads a frozen inference graph"""
@@ -218,14 +396,22 @@ class TLClassifier(object):
         return graph
 
     def detect(self, path_to_image):
-        # Load a sample image.
-        image = Image.open(path_to_image)
-        image_np = np.expand_dims(np.asarray(image, dtype=np.uint8), 0)
 
-        with tf.Session(graph=detection_graph) as sess:
+        # Load a sample image.
+        #image = Image.open(path_to_image)
+        image = cv2.imread(path_to_image)
+        #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        #print(image)
+        #image_np = np.asarray(image, dtype=np.uint8)
+        image_np = np.asarray(image)
+        print("After asarray")
+        print(image_np.shape)
+        image_np = np.expand_dims(image_np, 0)
+
+        with tf.Session(graph=self.detection_graph) as sess:
             # Actual detection.
-            (boxes, scores, classes) = sess.run([detection_boxes, detection_scores, detection_classes],
-                                                feed_dict={image_tensor: image_np})
+            (boxes, scores, classes) = sess.run([self.detection_boxes, self.detection_scores, self.detection_classes],
+                                                feed_dict={self.image_tensor: image_np})
 
             # Remove unnecessary dimensions
             boxes = np.squeeze(boxes)
@@ -234,18 +420,24 @@ class TLClassifier(object):
 
             confidence_cutoff = 0.8
             # Filter boxes with a confidence score less than `confidence_cutoff`
-            boxes, scores, classes = filter_boxes(confidence_cutoff, boxes, scores, classes)
+            boxes, scores, classes = self.filter_boxes(confidence_cutoff, boxes, scores, classes)
 
             # The current box coordinates are normalized to a range between 0 and 1.
             # This converts the coordinates actual location on the image.
-            width, height = image.size
-            box_coords = to_image_coords(boxes, height, width)
-
+            height = image_np.shape[0]
+            width = image_np.shape[1]
+            box_coords = self.to_image_coords(boxes, height, width)
+            print("Coordinates", box_coords)
+            print("classes", classes)
             # Each class with be represented by a differently colored box
-            draw_boxes(image, box_coords, classes)
+            self.draw_boxes(image, box_coords, classes)
 
             plt.figure(figsize=(12, 8))
             plt.imshow(image)
+            plt.show()
+
+        return image
+
 
     def time_detection(self, sess, img_height, img_width, runs=10):
         image_tensor = sess.graph.get_tensor_by_name('image_tensor:0')
@@ -265,35 +457,90 @@ class TLClassifier(object):
             times[i] = (t1 - t0) * 1000
         return times
 
-        def get_times(self):
-            with tf.Session(graph=detection_graph) as sess:
-                times = time_detection(sess, 600, 1000, runs=10)
+    def get_times(self):
+        with tf.Session(graph=detection_graph) as sess:
+            times = time_detection(sess, 600, 1000, runs=10)
 
-        def visualize_times(self):
-            # Create a figure instance
-            fig = plt.figure(1, figsize=(9, 6))
+    def visualize_times(self):
+        # Create a figure instance
+        fig = plt.figure(1, figsize=(9, 6))
 
-            # Create an axes instance
-            ax = fig.add_subplot(111)
-            plt.title("Object Detection Timings")
-            plt.ylabel("Time (ms)")
+        # Create an axes instance
+        ax = fig.add_subplot(111)
+        plt.title("Object Detection Timings")
+        plt.ylabel("Time (ms)")
 
-            # Create the boxplot
-            plt.style.use('fivethirtyeight')
-            bp = ax.boxplot(times)
+        # Create the boxplot
+        plt.style.use('fivethirtyeight')
+        bp = ax.boxplot(times)
+
+    def get_labels(self, filename="labels.csv", image_folder="data/*"):
+        labels=[]
+        f = open(filename, "w")
+
+        for name in glob.glob(image_folder):
+            labels.append(self.get_classification(cv2.imread(name)))
+            f.write(str(labels[-1])+",\n")
+        print(labels)
+        counter = collections.Counter(labels)
+        print(counter)
+        print("UNKNOWN=4, GREEN=2, YELLOW=1, RED=0")
+
+
+    def pipeline(self,img):
+        draw_img = Image.fromarray(img)
+        boxes, scores, classes = sess.run([self.detection_boxes, self.detection_scores, self.detection_classes], feed_dict={self.image_tensor: np.expand_dims(img, 0)})
+        # Remove unnecessary dimensions
+        boxes = np.squeeze(boxes)
+        scores = np.squeeze(scores)
+        classes = np.squeeze(classes)
+
+        confidence_cutoff = 0.8
+        # Filter boxes with a confidence score less than `confidence_cutoff`
+        boxes, scores, classes = self.filter_boxes(confidence_cutoff, boxes, scores, classes)
+
+        # The current box coordinates are normalized to a range between 0 and 1.
+        # This converts the coordinates actual location on the image.
+        width, height = draw_img.size
+        box_coords = self.to_image_coords(boxes, height, width)
+
+        # Each class with be represented by a differently colored box
+        self.draw_boxes(draw_img, box_coords, classes)
+        return np.array(draw_img)
 
 
 if __name__ == '__main__':
-    labels=[]
-    f = open("labels.csv", "w")
-    print(f)
-    cls = TLClassifier()
-    for name in glob.glob('data/*'):
-        #print(name)
-        labels.append(cls.get_classification(cv2.imread(name)))
-        f.write(str(labels[-1])+",\n")
 
-    print(labels)
-    counter = collections.Counter(labels)
-    print(counter)
-    print("UNKNOWN=4, GREEN=2, YELLOW=1, RED=0")
+    cls = TLClassifier()
+
+    #for name in glob.glob("data/left0025.jpg"):
+    # image = cls.detect("data/left0025.jpg")
+    # #print(image)
+    # plt.imshow(image)
+    # cv2.imwrite("sample1_boxes.jpg", image)
+    # plt.show()
+
+
+    #### Video
+    # clip = VideoFileClip('driving.mp4')
+    #
+    # with tf.Session(graph=cls.detection_graph) as sess:
+    #     image_tensor = sess.graph.get_tensor_by_name('image_tensor:0')
+    #     detection_boxes = sess.graph.get_tensor_by_name('detection_boxes:0')
+    #     detection_scores = sess.graph.get_tensor_by_name('detection_scores:0')
+    #     detection_classes = sess.graph.get_tensor_by_name('detection_classes:0')
+    #
+    #     new_clip = clip.fl_image(cls.pipeline)
+    #
+    #     # write to file
+    #     new_clip.write_videofile('result.mp4')
+
+    images = cls.load_and_train("train/")
+
+
+    # Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
+    # labeled_ds = list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+    # for image, label in labeled_ds.take(1):
+    #     print("Image shape: ", image.numpy().shape)
+    #     print("Label: ", label.numpy())
+    #display.display(Image.open(str(image_path)))
